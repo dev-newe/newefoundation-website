073c474904ada93dbb602ba2e7d93b7365372763
Anuvab Maity <162330937+AnuvabMaity@users.noreply.github.com>
2026-08-14 00:59:35 +0530

    fix: payload media upload access (#32)

--- src/db/collections/Media.ts
+++ src/db/collections/Media.ts
@@ -5,6 +5,9 @@ export const Media: CollectionConfig = {
   slug: "media",
   access: {
     read: () => true,
+    create: ({ req: { user } }) => Boolean(user),
+    update: ({ req: { user } }) => Boolean(user),
+    delete: ({ req: { user } }) => Boolean(user),
   },
   fields: [
     {
